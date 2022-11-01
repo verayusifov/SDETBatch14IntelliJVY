4901745c2f851d2ce619f9c12a9e48fc1d19e904
verayusifov <113564014+verayusifov@users.noreply.github.com>
2022-10-31 20:39:42 -0400

    Add files via upload

---
+++ src/Class24/ArrayListDemo3.java
@@ -0,0 +1,28 @@
+package Class24;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ArrayListDemo3 {
+    public static void main(String[] args) {
+        //how to store chars in arrayList
+       // ArrayList<Character> chars=new ArrayList<>(new ArrayList<>(Arrays.asList('E','F')));
+        // ArrayList<Character> chars=new ArrayList<>(new ArrayList<>(Arrays.asList('E','F')));
+        ArrayList<Character> chars=new ArrayList<>();
+        chars.add('A');
+        chars.add('B');
+        chars.add('C');
+        chars.add('Z');
+        chars.add('G');
+        System.out.println(chars);
+        ArrayList<Boolean> booleans=new ArrayList<>();
+        booleans.add(false);
+        booleans.add(false);
+        booleans.add(true);
+        System.out.println(booleans);
+        ArrayList<Float> floats=new ArrayList<>();
+        floats.add(12.5F);
+        floats.add(12f);
+        System.out.println(floats);
+    }
+}
